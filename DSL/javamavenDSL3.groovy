30a3077bb9207b0f71b5023cf7910168b544fac3
Gasteiz1921 <jamesko7lin@gmail.com>
2023-03-16 21:53:43 +0100

    Bengoetxea

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -59,7 +59,7 @@ job('Job test Hola Mundo') {
     	}    
 	steps {
 		shell('''
-			echo "Hola Mundo Jaime Altuna Morlesin ACEDO!!!!"
+			echo "Hola Mundo Jaime Altuna Morlesin ACEDO BENGOETXEA!!!!"
 		''')
 	}
 }
