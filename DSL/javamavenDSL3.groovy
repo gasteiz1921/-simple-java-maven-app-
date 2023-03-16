08b3f1554c388232a1e4d187476d4bea445d9368
Gasteiz1921 <jamesko7lin@gmail.com>
2023-03-16 21:54:49 +0100

    Noseque

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -59,7 +59,7 @@ job('Job test Hola Mundo') {
     	}    
 	steps {
 		shell('''
-			echo "Hola Mundo Jaime Altuna Morlesin ACEDO BENGOETXEA!!!!"
+			echo "Hola Mundo Jaime Altuna Morlesin ACEDO BENGOETXEA NOSEQUE!!!!"
 		''')
 	}
 }
